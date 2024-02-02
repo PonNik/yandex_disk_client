e544dca62308408bcd8217bc500f2a16a7b31da4
n.ponik <n.ponik@localhost>
2024-02-02 16:28:25 +0300

    change get token

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.ui.yadisk_console import YandexDiskConsole
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == '__main__':
-    # Замените 'YOUR_TOKEN' на ваш токен Яндекс.Диска
-    token = 'y0_AgAAAAA9XrX7AAst4wAAAAD5AN01yz_bCIHSQRW4wkChDGeuripnQAY'
+    token = os.getenv("TOKEN")
     yandex_disk_console = YandexDiskConsole(token)
     yandex_disk_console.run_console()
